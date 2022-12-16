343d2641a4c2c3c720804f6c9cbca0287af27a0f
brunoctt <brunotorresteles@gmail.com>
2022-12-15 22:18:04 -0300

    finding intersections of lines

--- lines_detection.py
+++ lines_detection.py
@@ -10,32 +10,52 @@ def show_image(img):
     plt.show()
 
 
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur,  50, 150)
-    return canny
+def get_orientation(line):
+    orientation = math.atan2(abs((line[3] - line[1])), abs((line[2] - line[0])))
+    return math.degrees(orientation)
 
 
-def biggest_distance(p1, p2):
-    def euclidian_dist(p):
-        return np.sqrt((p[0]-p[2])**2 + (p[1]-p[3])**2)
-    final_point = deepcopy(p1)
-    dist = euclidian_dist(p1)
-    for ind, val in enumerate(p2):
-        aux = deepcopy(final_point)
-        aux[ind] = val
-        new_dist = euclidian_dist(aux)
-        if new_dist > dist:
-            dist = new_dist
-            final_point = aux
+def distance_point_to_line(point, line):
+    px, py = point
+    x1, y1, x2, y2 = line
 
-    final_point[::2].sort()
-    final_point[1::2].sort()
-    return final_point
+    def line_magnitude(x1_, y1_, x2_, y2_):
+        line_mag = math.sqrt((x2_ - x1_) ** 2 + (y2_ - y1_) ** 2)
+        return line_mag
+
+    l_mag = line_magnitude(x1, y1, x2, y2)
+    if l_mag < 0.00000001:
+        distance_point_to_line = 9999
+        return distance_point_to_line
+
+    u1 = (((px - x1) * (x2 - x1)) + ((py - y1) * (y2 - y1)))
+    u = u1 / (l_mag * l_mag)
+
+    if (u < 0.00001) or (u > 1):
+        # closest point does not fall within the line segment, take the shorter distance
+        # to an endpoint
+        ix = line_magnitude(px, py, x1, y1)
+        iy = line_magnitude(px, py, x2, y2)
+        if ix > iy:
+            distance_point_to_line = iy
+        else:
+            distance_point_to_line = ix
+    else:
+        # Intersecting point is on the line, use the formula
+        ix = x1 + u * (x2 - x1)
+        iy = y1 + u * (y2 - y1)
+        distance_point_to_line = line_magnitude(px, py, ix, iy)
+
+    return distance_point_to_line
 
 
-import math
+def get_distance(line1, line2):
+    dist1 = distance_point_to_line(line1[:2], line2)
+    dist2 = distance_point_to_line(line1[2:], line2)
+    dist3 = distance_point_to_line(line2[:2], line1)
+    dist4 = distance_point_to_line(line2[2:], line1)
+
+    return min(dist1, dist2, dist3, dist4)
 
 
 class HoughBundler:
@@ -43,61 +63,16 @@ class HoughBundler:
         self.min_distance = min_distance
         self.min_angle = min_angle
 
-    def get_orientation(self, line):
-        orientation = math.atan2(abs((line[3] - line[1])), abs((line[2] - line[0])))
-        return math.degrees(orientation)
-
     def check_is_line_different(self, line_1, groups):
         for group in groups:
             for line_2 in group:
-                if self.get_distance(line_2, line_1) < self.min_distance:
-                    orientation_1 = self.get_orientation(line_1)
-                    orientation_2 = self.get_orientation(line_2)
+                if get_distance(line_2, line_1) < self.min_distance:
+                    orientation_1 = get_orientation(line_1)
+                    orientation_2 = get_orientation(line_2)
                     if abs(orientation_1 - orientation_2) < self.min_angle:
                         group.append(line_1)
                         return False
         return True
-
-    def distance_point_to_line(self, point, line):
-        px, py = point
-        x1, y1, x2, y2 = line
-
-        def line_magnitude(x1_, y1_, x2_, y2_):
-            line_mag = math.sqrt((x2_ - x1_) ** 2 + (y2_ - y1_) ** 2)
-            return line_mag
-
-        l_mag = line_magnitude(x1, y1, x2, y2)
-        if l_mag < 0.00000001:
-            distance_point_to_line = 9999
-            return distance_point_to_line
-
-        u1 = (((px - x1) * (x2 - x1)) + ((py - y1) * (y2 - y1)))
-        u = u1 / (l_mag * l_mag)
-
-        if (u < 0.00001) or (u > 1):
-            # closest point does not fall within the line segment, take the shorter distance
-            # to an endpoint
-            ix = line_magnitude(px, py, x1, y1)
-            iy = line_magnitude(px, py, x2, y2)
-            if ix > iy:
-                distance_point_to_line = iy
-            else:
-                distance_point_to_line = ix
-        else:
-            # Intersecting point is on the line, use the formula
-            ix = x1 + u * (x2 - x1)
-            iy = y1 + u * (y2 - y1)
-            distance_point_to_line = line_magnitude(px, py, ix, iy)
-
-        return distance_point_to_line
-
-    def get_distance(self, line1, line2):
-        dist1 = self.distance_point_to_line(line1[:2], line2)
-        dist2 = self.distance_point_to_line(line1[2:], line2)
-        dist3 = self.distance_point_to_line(line2[:2], line1)
-        dist4 = self.distance_point_to_line(line2[2:], line1)
-
-        return min(dist1, dist2, dist3, dist4)
 
     def merge_lines_into_groups(self, lines_list):
         groups = list()  # all lines groups are here
@@ -110,8 +85,9 @@ class HoughBundler:
 
         return groups
 
-    def merge_line_segments(self, lines):
-        orientation = self.get_orientation(lines[0])
+    @staticmethod
+    def merge_line_segments(lines):
+        orientation = get_orientation(lines[0])
 
         if len(lines) == 1:
             return np.block([[lines[0][:2], lines[0][2:]]])
@@ -134,7 +110,7 @@ class HoughBundler:
         lines_vertical = []
 
         for line_i in [l[0] for l in lines]:
-            orientation = self.get_orientation(line_i)
+            orientation = get_orientation(line_i)
             # if vertical
             if 45 < orientation <= 90:
                 lines_vertical.append(line_i)
@@ -176,7 +152,7 @@ contours = contours[idx]  # [x, y]
 img_contours = cv2.drawContours(img.copy(), contours, -1, (255, 0, 0), 10)
 
 """Refining Contours"""
-mask = np.zeros((1600, 901,3), dtype=np.uint8)
+mask = np.zeros((1600, 901, 3), dtype=np.uint8)
 contours_img = cv2.drawContours(mask, contours, -1, (255, 255, 255), 30)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 # contours_img = cv2.dilate(mask, kernel, iterations=8)
@@ -190,9 +166,9 @@ bordas = cv2.erode(binary, kernel, iterations=4)
 linhas = cv2.HoughLinesP(bordas, 1, np.pi / 180, thresh, minLineLength=min_line, maxLineGap=max_line)
 bordas_copia = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
 for linha in linhas:
-    x1, y1, x2, y2 = linha[0] # Retorna apenas dois pontos
+    x1, y1, x2, y2 = linha[0]  # Retorna apenas dois pontos
     cv2.line(bordas_copia, (x1, y1), (x2, y2), (255, 0, 0), 3)
-    
+
 """Merging Hough Lines and Contours"""
 merged = cv2.bitwise_and(contours_img, bordas_copia)
 merged = cv2.cvtColor(merged, cv2.COLOR_RGB2GRAY)
@@ -211,3 +187,21 @@ for linha in lines:
     cv2.line(bordas_copia, (x1, y1), (x2, y2), (255, 0, 0), 5)
 
 show_image(bordas_copia)
+
+intersections = []
+for i in range(len(lines)):
+    for j in range(len(lines)):
+        if i == j:
+            continue
+        l_i, l_j = lines[i][0], lines[j][0]
+        if abs((ang_i := get_orientation(l_i)) - (ang_j := get_orientation(l_j))) > 50:
+            if get_distance(l_i, l_j) < 10:
+                if {i, j} not in intersections:
+                    intersections.append({i, j})
+                    print(l_i, l_j, get_distance(l_i, l_j))
+                    continue
+            elif l_i[0] < l_j[0] < l_i[2] and l_j[1] < l_i[1] < l_j[3]:
+                if {i, j} not in intersections:
+                    intersections.append({i, j})
+                    print(l_i, l_j)
+                    continue
